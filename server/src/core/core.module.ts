e15d13afebc58e9945901e9a9997337e93a16c2d
firstPush <adel.sdet2@gmail.com>
2025-04-29 21:15:20 -0500

    week-3 - Build a basic module, model, and resolver - Generate and interact with the GraphQL schema - Test it via GraphQL Playground

--- server/src/core/core.module.ts
+++ server/src/core/core.module.ts
@@ -1,34 +1,33 @@
-import { ApolloDriver } from '@nestjs/apollo'
-import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { GraphQLModule } from '@nestjs/graphql'
-import { IS_DEV_ENV } from '../shared/utils/is-dev.util'
-import { getGraphQLConfig } from './config/graphql.config'
-import { PrismaModule } from 'src/prisma/prisma.module'
-import { RedisModule } from './redis/redis.module'
-import { HttpAdapterHost } from '@nestjs/core'
+import { ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { IS_DEV_ENV } from '../shared/utils/is-dev.util';
+import { getGraphQLConfig } from './config/graphql.config';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { RedisModule } from './redis/redis.module';
+import { HttpAdapterHost } from '@nestjs/core';
+import { AccountModule } from 'src/modules/auth/account/account.module'; // ✅ Import AccountModule
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            ignoreEnvFile: !IS_DEV_ENV,
-            isGlobal: true
-        }),
-        GraphQLModule.forRootAsync({
-            driver: ApolloDriver,
-            imports: [ConfigModule],
-            useFactory: getGraphQLConfig,
-            inject: [ConfigService]
-        }),
-        PrismaModule,
-        RedisModule,
-    ]
-    //,
-    // providers: [
-    //   HttpAdapterHost
-    // ]
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: !IS_DEV_ENV,
+      isGlobal: true,
+    }),
+    GraphQLModule.forRootAsync({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      useFactory: getGraphQLConfig,
+      inject: [ConfigService],
+    }),
+    PrismaModule,
+    AccountModule, // ✅ Add AccountModule to imports
+    RedisModule,
+  ],
 })
 export class CoreModule {}
+
 
 
 
