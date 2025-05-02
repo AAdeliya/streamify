55d6fa915b83c5362be6508d62454227f363a541
firstPush <adel.sdet2@gmail.com>
2025-05-01 21:48:55 -0500

    added apollo

--- server/src/modules/auth/account/account.service.ts
+++ server/src/modules/auth/account/account.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { UserModel } from './models/user.model';
+import { PrismaService } from 'src/prisma/prisma.service'
+
 
 @Injectable()
-export class AccountService {}
+export class AccountService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async findAll(): Promise<UserModel[]> {
+        const users = await this.prisma.user.findMany({
+          orderBy: {
+            createdAt: 'desc'
+          }
+        });
+    
+        return users.map(user => ({
+          ...user,
+          avatar: user.avatar || '',
+          bio: user.bio || ''
+        }));
+      }
+
+
+}
